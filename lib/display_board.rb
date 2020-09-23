093d0740cb5ceffb8aff4c95aa5f93a455806114
glorious-sequence-0954 <markvaughankay@gmail.com>
2020-09-23 05:51:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,20 +6,12 @@ board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
-
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
+board_1 = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
+display_board(board_1)
 
 

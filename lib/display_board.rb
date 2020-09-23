f22eee2c936468b46f914ed9954ede497f7d9416
glorious-sequence-0954 <markvaughankay@gmail.com>
2020-09-23 05:54:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,6 +11,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+display_board(board)
+
 board_1 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 display_board(board_1)
 
